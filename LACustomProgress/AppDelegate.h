de557bb57e59a81292ef96c5dfd7cba7e6ee5805
LiuQiqiang <lqq200912408@163.com>
2018-03-30 09:16:07 +0800

    Initial Commit

---
+++ LACustomProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LACustomProgress
+//
+//  Created by LiuQiqiang on 2018/3/30.
+//  Copyright © 2018年 LiuQiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LACustomProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LACustomProgress
+//
+//  Created by LiuQiqiang on 2018/3/30.
+//  Copyright © 2018年 LiuQiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
